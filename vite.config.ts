2e8d028f57d00c90aa7f9f75733ed883ad8332e0
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2022-03-05 22:32:04 +0100

    configuration: remove unused depDependecies

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import legacy from "@vitejs/plugin-legacy";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [legacy(), reactRefresh()],
+  plugins: [reactRefresh()],
   resolve: {
     alias: {
       // for TypeScript path alias import like : @/x/y/z
